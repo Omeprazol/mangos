57d322ca39f000407ee405565209a21bd376529c
przemratajczak <przemratajczak@wp.pl>
2010-05-22 16:15:15 +0200

    restored SD2 build after recent mangos update

--- src/bindings/ScriptDev2/scripts/kalimdor/temple_of_ahnqiraj/boss_skeram.cpp
+++ src/bindings/ScriptDev2/scripts/kalimdor/temple_of_ahnqiraj/boss_skeram.cpp
@@ -238,7 +238,7 @@ struct MANGOS_DLL_DECL boss_skeramAI : public ScriptedAI
                 break;
         }
 
-        for (int tryi = 0; tryi < 41; ++tryi)
+        /*for (int tryi = 0; tryi < 41; ++tryi)
         {
             Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM,0);
             if (pTarget && pTarget->GetTypeId() == TYPEID_PLAYER)
@@ -254,7 +254,7 @@ struct MANGOS_DLL_DECL boss_skeramAI : public ScriptedAI
                 }
                 break;
             }
-        }
+        }*/
 
         m_creature->RemoveAllAuras();
         m_creature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
