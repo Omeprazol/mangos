bbd4becad57d0b15de0e3c36f8cfcea2e96d5ffc
przemratajczak <przemratajczak@wp.pl>
2010-06-11 23:40:16 +0200

    [a537] stored working copy of Archavon the Stone Watcher for fouther development

--- src/bindings/ScriptDev2/scripts/northrend/vault_of_archavon/boss_archavon.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/vault_of_archavon/boss_archavon.cpp
@@ -24,17 +24,16 @@ EndScriptData */
 #include "precompiled.h"
 #include "vault_of_archavon.h"
 
-enum
+enum                                                 // cast    | durat.| effect    | eitA/B
 {
-    SPELL_ROCK_SHARDS_LEFT_N                = 58695,
-    SPELL_ROCK_SHARDS_LEFT_H                = 60883,
-    SPELL_ROCK_SHARDS_RIGHT_N               = 58696,
-    SPELL_ROCK_SHARDS_RIGHT_H               = 60884,
-    SPELL_CRUSHING_LEAP_N                   = 58963,
-    SPELL_CRUSHING_LEAP_H                   = 60895,
+    SPELL_ROCK_SHARDS_PERIODIC              = 58678, //  0.5s   |    3s | proc 58941|  6/0 
+    SPELL_CRUSHING_LEAP_N                   = 58960, //     0   |       | jump/missile| 53/0 87/0
+    SPELL_CRUSHING_LEAP_H                   = 60894,
     SPELL_STOMP_N                           = 58663,
     SPELL_STOMP_H                           = 60880,
-    SPELL_IMPALE_DMG_N                      = 58666,
+    SPELL_IMPALE_PERIODIC_SUMMON_AURA       = 53456,
+    SPELL_IMPALE_DMG_N                      = 58666, //     0   |       | sch dmg + sanctuary  | 6 / 0
+    SPELL_IMPALE                            = 58672, //     0   |   8   | control veh + stun | 38 / 0
     SPELL_IMPALE_DMG_H                      = 60882,
     SPELL_IMPALE_STUN                       = 50839,
     SPELL_BERSERK                           = 47008
@@ -45,64 +44,45 @@ struct MANGOS_DLL_DECL boss_archavonAI : public ScriptedAI
     boss_archavonAI(Creature* pCreature) : ScriptedAI(pCreature)
     {
         m_pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
-        m_bIsNormalMode = pCreature->GetMap()->IsRegularDifficulty();
-        m_fDefaultMoveSpeed = m_creature->GetSpeedRate(MOVE_RUN);
+        m_bIsRegularMode = pCreature->GetMap()->IsRegularDifficulty();
         Reset();
     }
 
     ScriptedInstance* m_pInstance;
-    bool m_bIsNormalMode;
-    float m_fDefaultMoveSpeed;
-    uint32 m_uiEvadeCheckCooldown;
+    bool m_bIsRegularMode;
 
     uint32 m_uiBerserkTimer;
     uint32 m_uiRockShardsTimer;
-    bool m_bRockShardsInProgress;
-    uint32 m_uiRockShardsProgressTimer;
-    uint32 m_uiRockShardTimer;
-    bool m_bRLRockShard;
-    Unit* m_pRockShardsTarget;
     uint32 m_uiCrushingLeapTimer;
-    Unit* m_pCrushingLeapTarget;
-    bool m_bCrushingLeapInProgress;
-    uint32 m_uiCrushingLeapSecureTimer;
     uint32 m_uiStompTimer;
-    uint32 m_uiImpaleAfterStompTimer;
-    bool m_bImpaleInProgress;
 
     void Reset()
     {
-        m_uiEvadeCheckCooldown = 2000;
-        m_creature->SetSpeedRate(MOVE_RUN, m_fDefaultMoveSpeed);
-        m_uiBerserkTimer = 300000;
-        m_uiRockShardsTimer = 15000;
-        m_bRockShardsInProgress = false;
-        m_uiRockShardsProgressTimer = 3000;
-        m_uiRockShardTimer = 0;
-        m_bRLRockShard = true;
-        m_pRockShardsTarget = NULL;
-        m_uiCrushingLeapTimer = 30000;
-        m_pCrushingLeapTarget = NULL;
-        m_bCrushingLeapInProgress = false;
-        m_uiCrushingLeapSecureTimer = 2000;
-        m_uiStompTimer = 45000;
-        m_uiImpaleAfterStompTimer = 1000;
-        m_bImpaleInProgress = false;
-
-        if(m_pInstance)
-            m_pInstance->SetData(TYPE_ARCHAVON, NOT_STARTED);
+        m_uiBerserkTimer        = 5 *MINUTE*IN_MILLISECONDS;
+        m_uiRockShardsTimer     = 20000;
+        m_uiCrushingLeapTimer   = 25000;
+        m_uiStompTimer          = 1 *MINUTE*IN_MILLISECONDS;
+        m_creature->RemoveAurasDueToSpell(SPELL_IMPALE_PERIODIC_SUMMON_AURA);
     }
 
     void Aggro(Unit *pWho)
     {
-        if(m_pInstance)
+        if (m_pInstance)
             m_pInstance->SetData(TYPE_ARCHAVON, IN_PROGRESS);
+
+        DoCastSpellIfCan(m_creature, SPELL_IMPALE_PERIODIC_SUMMON_AURA, CAST_TRIGGERED);
     }
 
     void JustDied(Unit* pKiller)
     {
-        if(m_pInstance)
+        if (m_pInstance)
             m_pInstance->SetData(TYPE_ARCHAVON, DONE);
+    }
+
+    void JustReachedHome()
+    {
+        if (m_pInstance)
+            m_pInstance->SetData(TYPE_ARCHAVON, FAIL);
     }
 
     void UpdateAI(const uint32 uiDiff)
@@ -110,154 +90,47 @@ struct MANGOS_DLL_DECL boss_archavonAI : public ScriptedAI
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
 
-        if (m_uiEvadeCheckCooldown < uiDiff)
+        if (m_uiBerserkTimer <= uiDiff)
         {
-            if (m_creature->GetDistance2d(-36.287f, -101.09f) > 80.0f)
-                EnterEvadeMode();
-            m_uiEvadeCheckCooldown = 2000;
+            DoCastSpellIfCan(m_creature, SPELL_BERSERK);
+            m_uiBerserkTimer = 30 * MINUTE * IN_MILLISECONDS;
         }
         else
-            m_uiEvadeCheckCooldown -= uiDiff;
+            m_uiBerserkTimer -= uiDiff;
 
-        if (m_bImpaleInProgress)
+        if (m_uiRockShardsTimer <= uiDiff)
         {
-            if (m_uiImpaleAfterStompTimer < uiDiff)
-            {
-                if (Unit* pTarget = m_creature->getVictim())
-                {
-                    DoCast(pTarget, m_bIsNormalMode ? SPELL_IMPALE_DMG_N : SPELL_IMPALE_DMG_H);
-                    pTarget->CastSpell(pTarget, SPELL_IMPALE_STUN, true);
-                }
-                m_bImpaleInProgress = false;
-            }
-            else
-            {
-                m_uiImpaleAfterStompTimer -= uiDiff;
-                return;
-            }
-        }
-
-        if (m_bCrushingLeapInProgress)
-        {
-            if (m_pCrushingLeapTarget)
-            {
-                if (m_pCrushingLeapTarget->isDead() || !m_pCrushingLeapTarget->IsInWorld() && !m_pCrushingLeapTarget->IsInMap(m_creature))
-                {
-                    m_bCrushingLeapInProgress = false;
-                    return;
-                }
-            }
-            else
-            {
-                m_bCrushingLeapInProgress = false;
-                return;
-            }
-            if ((m_uiCrushingLeapSecureTimer < uiDiff) || (m_pCrushingLeapTarget && m_creature->IsWithinDist(m_pCrushingLeapTarget, 5.0f)))
-            {
-                m_creature->getThreatManager().addThreat(m_pCrushingLeapTarget, -100000000.0f);
-                m_creature->SetSpeedRate(MOVE_RUN, m_fDefaultMoveSpeed);
-                DoCast(m_pCrushingLeapTarget, SPELL_IMPALE_DMG_H ? SPELL_CRUSHING_LEAP_N : SPELL_CRUSHING_LEAP_H, true);
-                m_bCrushingLeapInProgress = false;
-            }
-            else
-                m_uiCrushingLeapSecureTimer -= uiDiff;
-
-            return;
-        }
-
-        if (m_bRockShardsInProgress)
-        {
-            if (m_uiRockShardsProgressTimer < uiDiff)
-            {
-                m_bRockShardsInProgress = false;
-                if (m_pRockShardsTarget)
-                    m_creature->getThreatManager().addThreat(m_pRockShardsTarget, -100000000.0f);
-                return;
-            }
-            else
-                m_uiRockShardsProgressTimer -= uiDiff;
-
-            if (m_uiRockShardTimer < uiDiff)
-            {
-                if (m_pRockShardsTarget)
-                {
-                    DoCast(m_pRockShardsTarget, SPELL_CRUSHING_LEAP_H ? (m_bRLRockShard ? SPELL_ROCK_SHARDS_LEFT_N : SPELL_ROCK_SHARDS_RIGHT_N) : (m_bRLRockShard ? SPELL_ROCK_SHARDS_LEFT_H : SPELL_ROCK_SHARDS_RIGHT_H));
-                    m_bRLRockShard = !m_bRLRockShard;
-                }
-                m_uiRockShardTimer = 100;
-            }
-            else
-                m_uiRockShardsTimer -= uiDiff;
-
-            return;
-        }
-
-        if (m_uiRockShardsTimer < uiDiff)
-        {
-            m_bRockShardsInProgress = true;
-            m_uiRockShardsProgressTimer = 3000;
-            m_bRLRockShard = true;
-            m_pRockShardsTarget = NULL;
-            if (m_pRockShardsTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM,0))
-                m_creature->AddThreat(m_pRockShardsTarget, 1000.0f);
-            m_uiRockShardsTimer = 15000+rand()%15000;
-            return;
+            // according to YT vid target can be also pet
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+                DoCastSpellIfCan(pTarget, SPELL_ROCK_SHARDS_PERIODIC);
+            m_uiRockShardsTimer = 10000;
         }
         else
             m_uiRockShardsTimer -= uiDiff;
 
-        if (m_uiCrushingLeapTimer < uiDiff)
+        if (m_uiCrushingLeapTimer <= uiDiff)
         {
-            ThreatList const& threatlist = m_creature->getThreatManager().getThreatList();
-            if (threatlist.empty())
-                return;
-
-            ThreatList::const_iterator i = m_creature->getThreatManager().getThreatList().begin();
-            std::list<Unit*> lTargets;
-            for (; i != m_creature->getThreatManager().getThreatList().end(); ++i)
-            {
-                Unit* pTemp = Unit::GetUnit((*m_creature),(*i)->getUnitGuid());
-                if (pTemp && pTemp->GetTypeId() == TYPEID_PLAYER && !m_creature->IsWithinDist(pTemp, 10.0f) && m_creature->IsWithinDist(pTemp, 80.0f))
-                    lTargets.push_back(pTemp);
-            }
-            m_pCrushingLeapTarget = NULL;
-            if (!lTargets.empty())
-            {
-                std::list<Unit*>::iterator pTarget = lTargets.begin();
-                advance(pTarget, (rand() % lTargets.size()));
-                m_pCrushingLeapTarget = *pTarget;
-                if (m_pCrushingLeapTarget)
-                {
-                    m_creature->MonsterSay(m_pCrushingLeapTarget->GetName(), LANG_UNIVERSAL, NULL);
-                    m_creature->getThreatManager().addThreat(m_pCrushingLeapTarget, 100000000.0f);
-                    m_creature->SetSpeedRate(MOVE_RUN, m_fDefaultMoveSpeed*10.0f);
-                    m_bCrushingLeapInProgress = true;
-                    m_uiCrushingLeapSecureTimer = 2000;
-                }
-            }
-            m_uiCrushingLeapTimer = 30000+rand()%15000;
-            return;
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+                if (DoCastSpellIfCan(pTarget, m_bIsRegularMode ? SPELL_CRUSHING_LEAP_N : SPELL_CRUSHING_LEAP_H) == CAST_OK)
+                    m_uiCrushingLeapTimer = urand(30000, 35000);
         }
         else
             m_uiCrushingLeapTimer -= uiDiff;
 
-        if (m_uiStompTimer < uiDiff)
+        if (m_uiStompTimer <= uiDiff)
         {
-            DoCast(m_creature, m_bIsNormalMode ? SPELL_STOMP_N : SPELL_STOMP_H);
-            m_uiImpaleAfterStompTimer = 1000;
-            m_bImpaleInProgress = true;
-            m_uiStompTimer = 45000+rand()%15000;
+            Unit* pVictim = m_creature->getVictim();
+            if (DoCastSpellIfCan(m_creature->getVictim(), m_bIsRegularMode ? SPELL_STOMP_N : SPELL_STOMP_H) == CAST_OK)
+            {
+                // hack
+                float fPosX, fPosY, fPosZ;
+                m_creature->GetClosePoint(fPosX, fPosY, fPosZ, m_creature->GetObjectSize(), 50.0f);
+                pVictim->SendMonsterMove(fPosX, fPosY, fPosZ, SPLINETYPE_NORMAL, SPLINEFLAG_PITCH_UP, 7000);
+                m_uiStompTimer = 1 *MINUTE*IN_MILLISECONDS;
+            }
         }
         else
             m_uiStompTimer -= uiDiff;
-
-        if (m_uiBerserkTimer < uiDiff)
-        {
-            DoCast(m_creature, SPELL_BERSERK);
-            m_uiBerserkTimer = 60000;
-        }
-        else
-            m_uiBerserkTimer -= uiDiff;
 
         DoMeleeAttackIfReady();
     }
@@ -266,7 +139,7 @@ struct MANGOS_DLL_DECL boss_archavonAI : public ScriptedAI
 CreatureAI* GetAI_boss_archavon(Creature *pCreature)
 {
     return new boss_archavonAI (pCreature);
-};
+}
 
 void AddSC_boss_archavon()
 {
@@ -275,4 +148,4 @@ void AddSC_boss_archavon()
     newscript->Name = "boss_archavon";
     newscript->GetAI = &GetAI_boss_archavon;
     newscript->RegisterSelf();
-};
+}
