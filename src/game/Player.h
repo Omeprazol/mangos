41642773147d6f5c873569b05501973e7c368a85
przemratajczak <przemratajczak@wp.pl>
2010-06-16 23:40:36 +0200

    reverted [a253] Allow to change skin/hair color at Barber Shop

--- src/game/Player.h
+++ src/game/Player.h
@@ -1150,7 +1150,7 @@ class MANGOS_DLL_SPEC Player : public Unit
         std::string afkMsg;
         std::string dndMsg;
 
-        uint32 GetBarberShopCost(uint8 newhairstyle, uint8 newhaircolor, uint8 newfacialhair, BarberShopStyleEntry const* newSkin=NULL);
+        uint32 GetBarberShopCost(uint8 newhairstyle, uint8 newhaircolor, uint8 newfacialhair);
 
         PlayerSocial *GetSocial() { return m_social; }
 
