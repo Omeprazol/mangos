ea8278a727f8c8858a5cbab932432d7bc7537c87
przemratajczak <przemratajczak@wp.pl>
2010-06-24 12:47:12 +0200

    [a561] Finished support for Extinguish and FlameKeep quest/achevements of MFF (waiting for spawn sniffs)

--- DB_updates/a561_event_MidsummerFireFestival.sql
+++ DB_updates/a561_event_MidsummerFireFestival.sql
@@ -1,4 +1,5 @@
 -- Totem beacon focus on MFF bunnies
+DELETE FROM spell_script_target WHERE entry = 47129;
 INSERT INTO spell_script_target VALUES
 (47129,1,26258),
 (47129,1,26355);
@@ -154,26 +155,134 @@ UPDATE quest_template SET RequiredRaces = 1101 WHERE entry IN
 
 UPDATE creature_template SET npcflag = npcflag | 3 WHERE entry IN (25909,25892);
 
-/* spawns
--- alliance camps in WPL and Burning Steps
-DELETE FROM creature WHERE id IN (25892,25909);
+-- Honor the Flame! quests (for Horde, Eastern Kindomes)
+DELETE FROM creature_questrelation WHERE quest IN
+(11840,11842,11844,11848,11850,11853,11584,11837,11857,11860,11862);
+DELETE FROM creature_involvedrelation WHERE quest IN
+(11840,11842,11844,11848,11850,11853,11584,11837,11857,11860,11862);
+
+INSERT INTO creature_questrelation VALUES
+(25923,11840), -- Arathi Highlands
+(25925,11842), -- Badlands
+(25927,11844), -- Burning Steppes
+(25931,11848), -- Eversong Woods
+(25933,11850), -- Ghostlands
+(25935,11853), -- Hillsbrad Foothills
+(25939,11584), -- Silverpine Forest
+(25920,11837), -- Stranglethorn Vale
+(25941,11857), -- Swamp of Sorrows
+(25944,11860), -- The Hinterlands
+(25946,11862); -- Tirisfal Glades
+INSERT INTO creature_involvedrelation VALUES
+(25923,11840), -- Arathi Highlands
+(25925,11842), -- Badlands
+(25927,11844), -- Burning Steppes
+(25931,11848), -- Eversong Woods
+(25933,11850), -- Ghostlands
+(25935,11853), -- Hillsbrad Foothills
+(25939,11584), -- Silverpine Forest
+(25920,11837), -- Stranglethorn Vale
+(25941,11857), -- Swamp of Sorrows
+(25944,11860), -- The Hinterlands
+(25946,11862); -- Tirisfal Glades
+
+UPDATE quest_template SET RequiredRaces = 690 WHERE entry IN 
+(11840,11842,11844,11848,11850,11853,11584,11837,11857,11860,11862);
+
+UPDATE creature_template SET npcflag = npcflag | 3 WHERE entry IN (25931,25933,25946);
+
+############################################################################################################
+
+-- Desacrate Fire! quests (for Horde, Eastern Kingdoms)
+DELETE FROM gameobject_questrelation WHERE quest IN
+(11764,11766,11768,11772,11774,11776,11580,11801,11781,11786,11784);
+DELETE FROM gameobject_involvedrelation WHERE quest IN
+(11764,11766,11768,11772,11774,11776,11580,11801,11781,11786,11784);
+
+INSERT INTO gameobject_questrelation VALUES
+(187947,11764), -- Arathi Highlands
+(187954,11766), -- Badlands
+(187956,11768), -- Burning Steppes
+(194037,11772), -- Eversong Woods
+(194046,11774), -- Ghostlands
+(187964,11776), -- Hillsbrad Foothills
+(187559,11580), -- Silverpine Forest
+(187951,11801), -- Stranglethorn Vale
+(187969,11781), -- Swamp of Sorrows
+(187972,11784), -- The Hinterlands
+(194043,11786); -- Tirisfal Glades
+
+INSERT INTO gameobject_involvedrelation VALUES
+(187947,11764), -- Arathi Highlands
+(187954,11766), -- Badlands
+(187956,11768), -- Burning Steppes
+(194037,11772), -- Eversong Woods
+(194046,11774), -- Ghostlands
+(187964,11776), -- Hillsbrad Foothills
+(187559,11580), -- Silverpine Forest
+(187951,11801), -- Stranglethorn Vale
+(187969,11781), -- Swamp of Sorrows
+(187972,11784), -- The Hinterlands
+(194043,11786); -- Tirisfal Glades
+
+UPDATE quest_template SET RequiredRaces = 1101 WHERE entry IN 
+(11764,11766,11768,11772,11774,11776,11580,11801,11781,11786,11784);
+
+
+#############################################################################################################
+
+/*
+-- spawns that needs to be sniffed
+DELETE FROM creature WHERE id IN (25892,25909,25931,25933,25946);
 INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES
 ('9000021','25892','0','1','1','0','0','-8253.2','-2649.27','133.22','0.284419','300','0','0','1','0','0','0'),
-('9000022','25909','0','1','1','0','0','951.684','-1477.73','63.423','1.62985','300','0','0','1','0','0','0');
+('9000022','25909','0','1','1','0','0','951.684','-1477.73','63.423','1.62985','300','0','0','1','0','0','0'),
+('9000023','25931','530','1','1','0','0','9384.06','-6764.37','15.9061','4.52046','300','0','0','1','0','0','0'),
+('9000024','25933','530','1','1','0','0','7691.19','-6831.85','78.089','1.57364','300','0','0','1','0','0','0'),
+('9000025','25946','0','1','1','0','0','2270.72','390.175','34.6529','0.324528','300','0','0','1','0','0','0');
 
-DELETE FROM gameobject WHERE id IN (187929,187921);
+DELETE FROM gameobject WHERE id IN (187929,187921,194043,194037,194046);
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES
 ('200090','187921','0','1','1','962.293','-1480.73','62.8527','1.43351','0','0','0.65694','0.753943','60','0','1'),
-('200091','187929','0','1','1','-8246.02','-2641.63','133.155','3.55866','0','0','0.978336','-0.207026','60','0','1');
+('200091','187929','0','1','1','-8246.02','-2641.63','133.155','3.55866','0','0','0.978336','-0.207026','60','0','1'),
+('200094','194037','530','1','1','9374.44','-6765.66','15.588','4.69717','0','0','0.712466','-0.701707','60','0','1'),
+('200095','194043','0','1','1','2271.98','378.875','34.5583','6.0265','0','0','0.127989','-0.991776','60','0','1'),
+('200096','194046','530','1','1','7699.7','-6837.28','77.2783','1.30661','0','0','0.607812','0.794081','60','0','1');
 
-DELETE FROM gameobject WHERE guid IN (200092,200093);
+
+DELETE FROM gameobject WHERE guid IN (200092,200093,200097,200098,200099);
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES
 ('200092','188021','0','1','1','-8257.77','-2650.76','133.394','0.289328','0','0','0.14416','0.989554','25','0','1'),
-('200093','188021','0','1','1','953.291','-1482.58','64.0543','1.83013','0','0','0.792603','0.609738','25','0','1');
+('200093','188021','0','1','1','953.291','-1482.58','64.0543','1.83013','0','0','0.792603','0.609738','25','0','1'),
+('200097','188021','530','1','1','9384.33','-6761.54','16.672','4.68147','0','0','0.717953','-0.696092','25','0','1'),
+('200098','188021','530','1','1','7691.02','-6834.69','78.3759','1.49509','0','0','0.679842','0.733359','25','0','1'),
+('200099','188021','0','1','1','2266.48','388.578','35.7845','0.23028','0','0','0.114886','0.993379','25','0','1');
+
+DELETE FROM game_event_creature WHERE guid IN (9000021,9000022,9000023,9000024,9000025);
+INSERT INTO game_event_creature VALUES
+(9000021,1),
+(9000022,1),
+(9000023,1),
+(9000024,1),
+(9000025,1);
+
+DELETE FROM game_event_gameobject WHERE guid IN (200090,200091,200092,200093,200094,200095,200096,200097,200098,200099);
+INSERT INTO game_event_gameobject VALUES
+(200090,1),
+(200091,1),
+(200092,1),
+(200093,1),
+(200094,1),
+(200095,1),
+(200096,1),
+(200097,1),
+(200098,1),
+(200099,1);
 
 -- character cleanup
-DELETE FROM character_queststatus where quest in 
+DELETE FROM character_queststatus WHERE quest IN 
 (11804,11810,11814,11819,11822,11826,11827,11808,11813,11816,11820,11832,11828,11583,
-11761,11757,11581,11732,11739,11743,11748,11751,11755,11756,11737,11742,11745,11749); 
+11761,11757,11581,11732,11739,11743,11748,11751,11755,11756,11737,11742,11745,11749,
+11764,11766,11768,11772,11774,11776,11580,11801,11781,11786,11784
+11840,11842,11844,11848,11850,11853,11584,11837,11857,11860,11862); 
 */
-
